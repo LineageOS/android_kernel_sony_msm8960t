9efead99ce35199960b857f0e14b0a117569642a
Tarun Gupta <tarung@codeaurora.org>
2013-09-30 18:01:58 +0530

    USB: Debugfs: Fix compilation issues when debugfs is disabled
    When we disable debugfs some of the function driver does not
    handle those error properly so we see error of kind variable
    undeclared, too many arguements to function. This fixes all
    these obseved issue
    
     * [Adrian DC] Backport to legacy non-module u_serial.c
    
    Change-Id: I1b60c86b1c489f77edf86a5df44f8766a08dc117
    CRs-fixed: 551713
    Signed-off-by: Tarun Gupta <tarung@codeaurora.org>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- drivers/usb/gadget/f_rmnet.c
+++ drivers/usb/gadget/f_rmnet.c
@@ -1072,6 +1072,7 @@ static void frmnet_cleanup(void)
 	for (i = 0; i < nr_rmnet_ports; i++)
 		kfree(rmnet_ports[i].port);
 
+	gbam_cleanup();
 	nr_rmnet_ports = 0;
 	no_ctrl_smd_ports = 0;
 	no_data_bam_ports = 0;
