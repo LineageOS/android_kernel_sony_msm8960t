52f13bc808c2fa02db0b4b69a8c4bef376743898
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2017-10-09 12:59:56 -0700

    diag: Add mutex protection while reading dci debug statistics
    Unserialized access to diag_dbgfs_dci_data_index can lead to
    heap overflow. Add mutex protection while updating the
    diag_dbgfs_dci_data_index.
    
    Change-Id: Iee9d0447494e3576e6293afcd4d7611bc429aa8a
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/char/diag/diag_debugfs.c
+++ drivers/char/diag/diag_debugfs.c
@@ -28,6 +28,7 @@ static int diag_dbgfs_finished;
 static int diag_dbgfs_dci_data_index;
 static int diag_dbgfs_dci_finished;
 
+static struct mutex diag_dci_dbgfs_mutex;
 static ssize_t diag_dbgfs_read_status(struct file *file, char __user *ubuf,
 				      size_t count, loff_t *ppos)
 {
@@ -116,6 +117,7 @@ static ssize_t diag_dbgfs_read_dcistats(struct file *file,
 
 	bytes_remaining = buf_size;
 
+	mutex_lock(&diag_dci_dbgfs_mutex);
 	if (diag_dbgfs_dci_data_index == 0) {
 		bytes_written =
 			scnprintf(buf, buf_size,
@@ -152,6 +154,7 @@ static ssize_t diag_dbgfs_read_dcistats(struct file *file,
 	}
 
 	diag_dbgfs_dci_data_index = (i >= DIAG_DCI_DEBUG_CNT) ? 0 : i + 1;
+	mutex_unlock(&diag_dci_dbgfs_mutex);
 	bytes_written = simple_read_from_buffer(ubuf, count, ppos, buf,
 								bytes_in_buf);
 	kfree(buf);
@@ -610,6 +613,7 @@ void diag_debugfs_init(void)
 		pr_warn("diag: could not allocate memory for dci debug info\n");
 
 	mutex_init(&dci_stat_mutex);
+	mutex_init(&diag_dci_dbgfs_mutex);
 }
 
 void diag_debugfs_cleanup(void)
@@ -621,6 +625,7 @@ void diag_debugfs_cleanup(void)
 
 	kfree(dci_data_smd);
 	mutex_destroy(&dci_stat_mutex);
+	mutex_destroy(&diag_dci_dbgfs_mutex);
 }
 #else
 void diag_debugfs_init(void) { }
