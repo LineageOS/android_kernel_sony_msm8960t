69608cb74527f3e9cf771a129990d0a102b386d2
Adrian DC <radian.dc@gmail.com>
2019-08-08 16:58:35 +0200

    Revert "mm: Fix "mm: vmpressure: account allocstalls only on higher pressures""
    This reverts commit 29034ebf53b6f3d91491735c3cc29a7c224e67ff.
    
    Change-Id: I07e66aa3d061bfd681673a5eb8460160eef737f2

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -57,11 +57,6 @@ static unsigned long vmpressure_scale_max = 100;
 module_param_named(vmpressure_scale_max, vmpressure_scale_max,
 			ulong, S_IRUGO | S_IWUSR);
 
-/* vmpressure values >= this will be scaled based on allocstalls */
-static unsigned long allocstall_threshold = 70;
-module_param_named(allocstall_threshold, allocstall_threshold,
-			ulong, S_IRUGO | S_IWUSR);
-
 static struct vmpressure global_vmpressure;
 BLOCKING_NOTIFIER_HEAD(vmpressure_notifier);
 
@@ -176,7 +171,7 @@ static unsigned long vmpressure_account_stall(unsigned long pressure,
 {
 	unsigned long scale;
 
-	if (pressure < allocstall_threshold)
+	if (pressure < 70)
 		return pressure;
 
 	scale = ((vmpressure_scale_max - pressure) * stall) / scanned;
